f9317910a6ce94971878e5d31729811beda4ecdb
Демидов Кирилл <kirill.demidov@gazprombank.ru>
2025-05-14 14:25:11 +0300

    Initial commit

---
+++ src/main/java/com/search/postgres/prilka/PrilkaApplication.java
@@ -0,0 +1,13 @@
+package com.search.postgres.prilka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrilkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrilkaApplication.class, args);
+	}
+
+}
